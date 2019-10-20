459895b0302e4269218c0e9e8013e378d29ce249
huynhlv <huynhlevan1997@gmail.com>
2019-10-20 19:20:18 +0700

    test evenbus

---
+++ src/components/HelloDac.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <select v-model="selected">
+      <option v-for="(option, index) in options" :key="index" v-bind:value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+    <span>Selected: {{ selected }}</span>
+  </div>
+</template>
+
+<script>
+import EventBus from '@/eventBus'
+export default {
+  data () {
+    return {
+      selected: 'A',
+      options: [
+        { text: 'One', value: 'A' },
+        { text: 'Two', value: 'B' },
+        { text: 'Three', value: 'C' }
+      ],
+      nameDac: 'ddd'
+    }
+  },
+  watch: {
+    selected() {
+      this.sendEventBus()
+    }
+  },
+  methods: {
+    sendEventBus() {
+      EventBus.$emit('hello', this.selected);
+    }
+  }
+}
+</script>
---
+++ src/components/HelloNeo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ nameNeo }}
+  </div>
+</template>
+
+<script>
+import EventBus from '@/eventBus'
+export default {
+  data () {
+    return {
+      nameNeo: 'nnnn'
+    }
+  },
+  created() {
+    // Listening the event hello
+    EventBus.$on('hello', this.handler);
+  },
+  destroyed() {
+    // Stop listening the event hello with handler
+    EventBus.$off('hello', this.handler);
+  },
+  methods: {
+    handler(e) {
+      this.nameNeo = e
+      alert(e)
+    }
+  }
+}
+</script>
